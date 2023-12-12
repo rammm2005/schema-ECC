bfe80bfab38ae9ab80947e1e5cbf58fc58ac20d2
rammm2005 <ditarama985@gmail.com>
2023-12-12 21:04:18 +0700

    add the case off the DML syntac and DDL syntac SQL

--- schema.db.sql
+++ schema.db.sql
@@ -148,9 +148,9 @@ INSERT INTO penjual (id_penjual, nama_penjual, id_alamat, email, no_tlp, jenis_k
   ('P002', 'Penjual 2', 2, 'penjual2@example.com', '9876543210', 'cewek', 1, 'Jalan B 1');
 
 -- Insert data into the "barang" table
-INSERT INTO barang (id_barang, merk, harga, deskripsi, id_kategori, id_penjual) VALUES
-  ('B001', 'Merk A', 100.0, 'Deskripsi Barang A', 'K001', 'P001'),
-  ('B002', 'Merk B', 150.0, 'Deskripsi Barang B', 'K002', 'P002');
+INSERT INTO barang (id_barang, merk, harga,stok, deskripsi, id_kategori, id_penjual) VALUES
+  ('B001', 'Merk A', 100.0,100, 'Deskripsi Barang A', 'K001', 'P001'),
+  ('B002', 'Merk B', 150.0,150, 'Deskripsi Barang B', 'K002', 'P002');
 
 -- Insert data into the "pelanggan" table
 INSERT INTO pelanggan (id_pelanggan, nama_pelanggan, tgl_lahir, jenis_kelamin, id_alamat, email, no_tlp) VALUES
@@ -181,3 +181,130 @@ INSERT INTO orders (id_order, jumlah_order, total_harga, alamat_tujuan, tgl_orde
 INSERT INTO jasa_pengiriman (kode_pengiriman, nama_pengiriman, status) VALUES
   ('JP001', 'Jasa Pengiriman A', 1),
   ('JP002', 'Jasa Pengiriman B', 1);
+
+
+-- 1. Average Rating for Each Product
+SELECT id_barang, AVG(bintang) AS avg_rating
+FROM rating
+GROUP BY id_barang;
+
+-- 2. Average Price of Products by Category
+SELECT K.nama_kategori, AVG(B.harga) AS avg_price
+FROM barang B
+JOIN kategori K ON B.id_kategori = K.id_kategori
+GROUP BY K.nama_kategori;
+
+-- 3. Average Order Quantity
+SELECT AVG(jumlah_order) AS avg_order_quantity
+FROM orders;
+
+-- 4. Average Order Total Price
+SELECT AVG(total_harga) AS avg_order_total_price
+FROM orders;
+
+-- 5. Average Price of Products Sold by Each Seller
+SELECT P.nama_penjual, AVG(B.harga) AS avg_price
+FROM barang B
+JOIN penjual P ON B.id_penjual = P.id_penjual
+GROUP BY P.nama_penjual;
+
+-- 6. Average Number of Orders per Customer
+SELECT id_pelanggan, AVG(jumlah_order) AS avg_orders_per_customer
+FROM orders
+GROUP BY id_pelanggan;
+
+-- 7. Average Rating for Each Category
+SELECT K.nama_kategori, AVG(R.bintang) AS avg_rating
+FROM kategori K
+LEFT JOIN barang B ON K.id_kategori = B.id_kategori
+LEFT JOIN rating R ON B.id_barang = R.id_barang
+GROUP BY K.nama_kategori;
+
+-- 8. Average Price of Products Sold by Each Seller in Each Category
+SELECT P.nama_penjual, K.nama_kategori, AVG(B.harga) AS avg_price
+FROM barang B
+JOIN penjual P ON B.id_penjual = P.id_penjual
+JOIN kategori K ON B.id_kategori = K.id_kategori
+GROUP BY P.nama_penjual, K.nama_kategori;
+
+-- 9. Average Rating by Gender for Each Product
+SELECT R.id_barang, P.jenis_kelamin, AVG(R.bintang) AS avg_rating
+FROM rating R
+JOIN pelanggan P ON R.id_pelanggan = P.id_pelanggan
+GROUP BY R.id_barang, P.jenis_kelamin;
+
+-- 10. Average Order Quantity by Product
+SELECT id_barang, AVG(jumlah_order) AS avg_order_quantity
+FROM orders
+GROUP BY id_barang;
+
+-- 11. Average Price of Products Sold by Each Seller by Gender
+SELECT P.nama_penjual, Pel.jenis_kelamin, AVG(B.harga) AS avg_price
+FROM barang B
+JOIN penjual P ON B.id_penjual = P.id_penjual
+JOIN pelanggan Pel ON P.id_penjual = Pel.id_alamat
+GROUP BY P.nama_penjual, Pel.jenis_kelamin;
+
+-- 12. Average Order Quantity by Seller
+SELECT id_penjual, AVG(jumlah_order) AS avg_order_quantity
+FROM orders
+GROUP BY id_penjual;
+
+-- 13. Average Price of Products Sold in Each Province
+SELECT A.provinsi, AVG(B.harga) AS avg_price
+FROM barang B
+JOIN penjual P ON B.id_penjual = P.id_penjual
+JOIN alamat A ON P.id_alamat = A.id_alamat
+GROUP BY A.provinsi;
+
+-- 14. Average Order Total Price by Seller
+SELECT id_penjual, AVG(total_harga) AS avg_order_total_price
+FROM orders
+GROUP BY id_penjual;
+
+-- 15. Average Price of Products Sold in Each City
+SELECT A.kabupaten, AVG(B.harga) AS avg_price
+FROM barang B
+JOIN penjual P ON B.id_penjual = P.id_penjual
+JOIN alamat A ON P.id_alamat = A.id_alamat
+GROUP BY A.kabupaten;
+
+-- 16. Average Rating for Each Product by Gender
+SELECT R.id_barang, Pel.jenis_kelamin, AVG(R.bintang) AS avg_rating
+FROM rating R
+JOIN pelanggan Pel ON R.id_pelanggan = Pel.id_pelanggan
+GROUP BY R.id_barang, Pel.jenis_kelamin;
+
+-- 17. Average Price of Products Sold by Each Seller in Each City
+SELECT P.nama_penjual, A.kabupaten, AVG(B.harga) AS avg_price
+FROM barang B
+JOIN penjual P ON B.id_penjual = P.id_penjual
+JOIN alamat A ON P.id_alamat = A.id_alamat
+GROUP BY P.nama_penjual, A.kabupaten;
+
+-- 18. Average Rating by Age Group for Each Product
+SELECT R.id_barang, 
+    CASE 
+        WHEN YEAR(CURRENT_DATE) - YEAR(Pel.tgl_lahir) BETWEEN 18 AND 30 THEN '18-30'
+        WHEN YEAR(CURRENT_DATE) - YEAR(Pel.tgl_lahir) BETWEEN 31 AND 45 THEN '31-45'
+        ELSE '46+'
+    END AS age_group,
+    AVG(R.bintang) AS avg_rating
+FROM rating R
+JOIN pelanggan Pel ON R.id_pelanggan = Pel.id_pelanggan
+GROUP BY R.id_barang, age_group;
+
+-- 19. Average Order Quantity by Category
+SELECT K.nama_kategori, AVG(jumlah_order) AS avg_order_quantity
+FROM orders O
+JOIN barang B ON O.id_barang = B.id_barang
+JOIN kategori K ON B.id_kategori = K.id_kategori
+GROUP BY K.nama_kategori;
+
+-- 20. Average Rating by Payment Type for Each Product
+SELECT R.id_barang, JP.tipe_pembayaran, AVG(R.bintang) AS avg_rating
+FROM rating R
+JOIN jenis_pembayaran JP ON R.id_pembayaran = JP.id_pembayaran
+GROUP BY R.id_barang, JP.tipe_pembayaran;
+
+
