ef7add897bf3665dd9fcedddb2e04fd219d65f5f
rammm2005 <ditarama985@gmail.com>
2023-12-15 23:13:30 +0700

    push fix

--- schema.db.sql
+++ schema.db.sql
@@ -65,7 +65,7 @@ CREATE TABLE penjual (
   FOREIGN KEY(id_alamat) REFERENCES alamat(id_alamat) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE produk (
-  id_produk CHAR(5) NOT NULL,
+  id_produk CHAR(6) NOT NULL,
   merk VARCHAR(50) NOT NULL,
   harga DECIMAL(11) NOT NULL,
   deskripsi TEXT NOT NULL,
@@ -93,7 +93,7 @@ CREATE TABLE pelanggan(
 );
 CREATE TABLE rating(
   id_rating INT(11) NOT NULL AUTO_INCREMENT,
-  id_produk CHAR(5) NOT NULL,
+  id_produk CHAR(6) NOT NULL,
   ulasan TEXT NOT NULL,
   bintang INT(5) NOT NULL,
   id_pelanggan CHAR(5) NOT NULL,
@@ -127,7 +127,7 @@ CREATE TABLE orders(
   alamat_tujuan VARCHAR(100) NOT NULL,
   tgl_order DATE NOT NULL,
   id_pembayaran CHAR(5) NOT NULL,
-  id_produk CHAR(5) NOT NULL,
+  id_produk CHAR(6) NOT NULL,
   id_kurir CHAR(5) NOT NULL,
   id_penjual CHAR(5) NOT NULL,
   id_pelanggan CHAR(5) NOT NULL,
@@ -165,50 +165,110 @@ MODIFY COLUMN bintang DECIMAL(3,2) NOT NULL;
 
 -- Insert Command
 INSERT INTO kategori(id_kategori, nama_kategori)
-VALUES ('J0001', 'HandPhone'),
-  ('J0002', 'Laptop'),
-  ('J0003', 'Tablet'),
-  ('J0004', 'Aksesoris Handphone'),
-  ('J0005', 'Personal Computer');
--- Insert into provinsi
-INSERT INTO provinsi (nama_provinsi)
-VALUES ('Bali'),('jakarta');
--- Insert into kabupaten
-INSERT INTO kabupaten (nama_kabupaten)
-VALUES ('Badung'),
-  ('Bangli'),
-  ('Buleleng'),
-  ('Gianyar'),
-  ('Jembrana'),
-  ('Karangasem'),
-  ('Klungkung'),
-  ('Jakarta Selatan'),
-  ('Tabanan');
--- Insert into kecamatan
-INSERT INTO kecamatan (nama_kecamatan)
-VALUES ('Kuta'),
-    ('Menteng'),
-  ('Petang');
--- Insert into desa_kelurahan
-INSERT INTO desa_kelurahan (nama_desa_kelurahan)
-VALUES ('Kuta'),
-('Cikini'),
-  ('Legian');
--- Insert into kode_pos
-INSERT INTO kode_pos (kode_pos)
-VALUES (80361),
-    (1230),
-  -- Kuta
-  (80361);
--- Legian
+VALUES 
+ ('J0003','Mother Board'),
+  ('J0004', 'Apple Phone'),
+  ('J0005','Air Pods'),
+  ('J0006', 'Smartwatch'),
+  ('J0007', 'Printer'),
+  ('J0008', 'Gaming Laptop'),
+  ('J0009', 'Power Bank'),
+  ('J0010', 'Camera'),
+  ('J0011', 'Headphones'),
+  ('J0012', 'Fitness Tracker'),
+  ('J0013', 'External Hard Drive'),
+  ('J0014', 'Wireless Charger'),
+  ('J0015', 'Computer Accessories');
 
+-- Insert additional data into provinsi
+INSERT INTO provinsi (nama_provinsi)
+VALUES 
+  ('Aceh'),
+  ('Sumatera Utara'),
+  ('Sumatera Barat'),
+  ('Riau'),
+  ('Jambi'),
+  ('Sumatera Selatan'),
+  ('Bengkulu'),
+  ('Lampung'),
+  ('Bangka Belitung'),
+  ('Kalimantan Barat');
+
+-- Insert additional data into kabupaten
+INSERT INTO kabupaten (nama_kabupaten)
+VALUES 
+  ('Aceh Barat'),
+  ('Tapanuli Selatan'),
+  ('Pasaman'),
+  ('Kampar'),
+  ('Muaro Jambi'),
+  ('Ogan Komering Ilir'),
+  ('Bengkulu Tengah'),
+  ('Lampung Selatan'),
+  ('Bangka'),
+  ('Pontianak');
+
+-- Insert additional data into kecamatan
+INSERT INTO kecamatan (nama_kecamatan)
+VALUES 
+  ('Meulaboh'),
+  ('Balige'),
+  ('Lubuk Sikaping'),
+  ('Bukit Batu'),
+  ('Jambi Timur'),
+  ('Palembang Utara'),
+  ('Curup'),
+  ('Metro'),
+  ('Manggar'),
+  ('Singkawang');
+
+-- Insert additional data into desa_kelurahan
+INSERT INTO desa_kelurahan (nama_desa_kelurahan)
+VALUES 
+  ('Gampong Baro'),
+  ('Huta Bulu'),
+  ('Sungai Liput'),
+  ('Pulau Muda'),
+  ('Tanjungkarang'),
+  ('Ilir Barat I'),
+  ('Batu Tanam'),
+  ('Gedong Tataan'),
+  ('Manggar Baru'),
+  ('Dompak');
+
+-- Insert additional data into kode_pos
+INSERT INTO kode_pos (kode_pos)
+VALUES 
+  (23456),
+  (56789),
+  (12345),
+  (67890),
+  (34567),
+  (89012),
+  (45678),
+  (90123),
+  (98765),
+  (32109);
+
+-- Insert additional data into alamat
 INSERT INTO alamat(
   id_provinsi ,
   id_kabupaten ,
   id_kode_pos ,
   id_kecamatan ,
-  id_desa_kelurahan) VALUES (1,1,1,1,1), (2,8,2,2,2);
--- Insert into penjual
+  id_desa_kelurahan) VALUES 
+  (3, 4, 3, 4, 4),
+  (1, 9, 8, 2, 1),
+  (6, 3, 5, 5, 6),
+  (7, 1, 2, 8, 7),
+  (2, 10, 7, 3, 2),
+  (5, 6, 1, 6, 5),
+  (8, 2, 4, 9, 8),
+  (4, 5, 6, 1, 10),
+  (9, 7, 9, 7, 3),
+  (10, 8, 10, 10, 9);
+
+-- Insert additional data into penjual
 INSERT INTO penjual (
     id_penjual,
     nama_penjual,
@@ -219,52 +279,42 @@ INSERT INTO penjual (
     jenis_kelamin,
     status_penjual
   )
-VALUES (
-    'P1000',
-    'Toko ABC',
-    'Jalan Raya No. 123',
-    1,
-    'tokoabc@email.com',
-    '08123456789',
-    'cowok',
-    1
-  ),
-  (
-    'P2000',
-    'Toko XYZ',
-    'Jalan Utama No. 456',
-    2,
-    'tokoxyx@email.com',
-    '08234567890',
-    'cewek',
-    1
-  );
--- Insert into produk
+VALUES 
+  ('P3000', 'Toko EFG', 'Jalan Maju No. 789', 3, 'tokoeft@email.com', '08765432109', 'cowok', 1),
+  ('P4000', 'Toko LMN', 'Jalan Sejahtera No. 1011', 4, 'tokolmn@email.com', '08901234567', 'cewek', 1),
+  ('P5000', 'Toko OPQ', 'Jalan Harmoni No. 1213', 5, 'tokoopq@email.com', '08123456789', 'cowok', 1),
+  ('P6000', 'Toko RST', 'Jalan Indah No. 1415', 6, 'tokorst@email.com', '08234567890', 'cewek', 1),
+  ('P7000', 'Toko UVW', 'Jalan Damai No. 1617', 7, 'tokouvw@email.com', '08345678901', 'cowok', 1),
+  ('P8000', 'Toko XYZ', 'Jalan Sentosa No. 1819', 8, 'tokowxy@email.com', '08456789012', 'cewek', 1),
+  ('P9000', 'Toko ABC', 'Jalan Bersama No. 2021', 9, 'tokoabc@email.com', '08567890123', 'cowok', 1),
+  ('P1000', 'Toko DEF', 'Jalan Kencana No. 2223', 10, 'tokodef@email.com', '08678901234', 'cewek', 1),
+  ('P1100', 'Toko GHI', 'Jalan Sejati No. 2425', 1, 'tokoghi@email.com', '08789012345', 'cowok', 1),
+  ('P1200', 'Toko JKL', 'Jalan Bahagia No. 2627', 2, 'tokojkl@email.com', '08890123456', 'cewek', 1);
+
+
+-- Insert additional data into produk
 INSERT INTO produk (
     id_produk,
     merk,
     harga,
+    stok,
     deskripsi,
     id_kategori,
     id_penjual
   )
-VALUES (
-    'PR1',
-    'Produk A',
-    100000,
-    'Deskripsi Produk A',
-    'J0002',
-    'P1000'
-  ),
-  (
-    'PR2',
-    'Produk B',
-    150000,
-    'Deskripsi Produk B',
-    'J0001',
-    'P2000'
-  );
--- Insert into pelanggan
+VALUES 
+  ('PR0003', 'ROG mother Board', 1200000,100, 'mother board ini sangatlah keren dan kualitas kerja tenga kuda', 'J0003', 'P3000'),
+  ('PR0004', 'Iphone 15 +', 8000000,140, 'Design stylish dengan tampilan elegan dan juga luxury dan royal', 'J0004', 'P4000'),
+  ('PR0005', 'Air Pods Pro Gen 2', 2500000,50 ,'memiliki ke luaran suara yang lebih jernih dan mengagumkan ketika mendengar', 'J0005', 'P5000'),
+  ('PR0006', 'Smart Watch Gen 3', 3000000,25 ,'Memiliki design yang elegan dan multi fungsi baru', 'J0006', 'P6000'),
+  ('PR0007', 'Printer LG', 1800000,30 ,'Memiliki tampilan yang lebih keren dan kinerja dan hasil tinta yang memukau', 'J0007', 'P7000'),
+  ('PR0008', 'ROG Strik', 50000000,170 ,'Laptop Gaming yang memiliki design yang elegan dan performa yang stabil ketika gaming', 'J0008', 'P8000'),
+  ('PR0009', 'Robots', 2000000,200, 'Memiliki kapasitas batery yang ebih kuat dan daya charge yang lebih tinggi', 'J0009', 'P9000'),
+  ('PR0010', 'Sony 3', 1500000,36, 'Memiliki tampilan gambar yang keren dan jernih', 'J0010', 'P1000'),
+  ('PR0011', 'POCO X 4', 6500000,11, 'Performa gaming yang bagus dengan harga yang murah untuk itu worth it', 'J0011', 'P1100'),
+  ('PR0012', 'Fitness Pro', 120000,35, 'Tracking performa anda untuk sesuatu yang memukau', 'J0012', 'P1200');
+
+-- Insert additional data into pelanggan
 INSERT INTO pelanggan (
     id_pelanggan,
     nama_pelanggan,
@@ -275,43 +325,67 @@ INSERT INTO pelanggan (
     email,
     no_tlp
   )
-VALUES (
-    'C1000',
-    'Pelanggan 1',
-    'Jalan Pelanggan 1',
-    '2000-01-01',
-    'cowok',
-    1,
-    'pelanggan1@email.com',
-    '08345678901'
-  ),
-  (
-    'C2000',
-    'Pelanggan 2',
-    'Jalan Pelanggan 2',
-    '1995-05-15',
-    'cewek',
-    2,
-    'pelanggan2@email.com',
-    '08456789012'
-  );
--- Insert into rating
+VALUES 
+  ('C3000', 'Pelanggan 3', 'Jalan Pelanggan 3', '1998-08-25', 'cewek', 3, 'pelanggan3@email.com', '08901234567'),
+  ('C4000', 'Pelanggan 4', 'Jalan Pelanggan 4', '2001-03-10', 'cowok', 4, 'pelanggan4@email.com', '08123456789'),
+  ('C5000', 'Pelanggan 5', 'Jalan Pelanggan 5', '1990-12-01', 'cewek', 5, 'pelanggan5@email.com', '08234567890'),
+  ('C6000', 'Pelanggan 6', 'Jalan Pelanggan 6', '1985-05-20', 'cowok', 6, 'pelanggan6@email.com', '08345678901'),
+  ('C7000', 'Pelanggan 7', 'Jalan Pelanggan 7', '1999-11-15', 'cewek', 7, 'pelanggan7@email.com', '08456789012'),
+  ('C8000', 'Pelanggan 8', 'Jalan Pelanggan 8', '1993-02-28', 'cowok', 8, 'pelanggan8@email.com', '08567890123'),
+  ('C9000', 'Pelanggan 9', 'Jalan Pelanggan 9', '1988-09-05', 'cewek', 9, 'pelanggan9@email.com', '08678901234'),
+  ('C1000', 'Pelanggan 10', 'Jalan Pelanggan 10', '2002-07-18', 'cowok', 10, 'pelanggan10@email.com', '08789012345'),
+  ('C1100', 'Pelanggan 11', 'Jalan Pelanggan 11', '1997-04-03', 'cewek', 1, 'pelanggan11@email.com', '08890123456'),
+  ('C1200', 'Pelanggan 12', 'Jalan Pelanggan 12', '1980-10-12', 'cowok', 2, 'pelanggan12@email.com', '08901234567');
+
+-- Insert additional data into rating
 INSERT INTO rating (id_produk, ulasan, bintang, id_pelanggan)
-VALUES ('PR1', 'Produk A bagus', 5, 'C1000'),
-  ('PR2', 'Produk B kurang memuaskan', 3, 'C2000');
--- Insert into kurir
+VALUES 
+  ('PR0003', 'Produk C luar biasa', 5, 'C3000'),
+  ('PR0004', 'Produk D sangat baik', 4.5, 'C4000'),
+  ('PR0005', 'Produk E bagus', 5, 'C5000'),
+  ('PR0006', 'Produk F kurang memuaskan', 3, 'C6000'),
+  ('PR0007', 'Produk G oke banget', 5, 'C7000'),
+  ('PR0008', 'Produk H lumayan', 4.3, 'C8000'),
+  ('PR0009', 'Produk I biasa saja', 3.7, 'C9000'),
+  ('PR0010', 'Produk J puas', 5, 'C1000'),
+  ('PR0011', 'Produk K tidak sesuai ekspektasi', 2.7, 'C1100'),
+  ('PR0012', 'Produk L mengecewakan', 1.2, 'C1200');
+
+-- Insert additional data into kurir
 INSERT INTO kurir (id_kurir, nama_kurir, alamat, status_kurir)
-VALUES ('K1000', 'JNE Express', 'Jalan Ekspres No. 1', 1),
-  ('K2000', 'Pos Indonesia', 'Jalan Pos No. 2', 1);
--- Insert into jenis_pembayaran
+VALUES 
+  ('K3000', 'Tiki Express', 'Jalan Cepat No. 3', 1),
+  ('K4000', 'Lion Parcel', 'Jalan Singa No. 4', 1),
+  ('K5000', 'J&T Logistics', 'Jalan Tuntas No. 5', 1),
+  ('K6000', 'Sicepat Express', 'Jalan Cepat No. 6', 1),
+  ('K7000', 'Wahana Ekspres', 'Jalan Wahana No. 7', 1),
+  ('K8000', 'Ninja Xpress', 'Jalan Ninja No. 8', 1),
+  ('K9000', 'AnterAja', 'Jalan Antar No. 9', 1),
+  ('K1000', 'SiCepat Ekspres', 'Jalan Cepat No. 10', 1),
+  ('K1100', 'JNE Logistics', 'Jalan Ekspres No. 11', 1),
+  ('K1200', 'Pos Express', 'Jalan Pos No. 12', 1);
+
+
+
+-- Insert additional data into jenis_pembayaran
 INSERT INTO jenis_pembayaran (
     id_pembayaran,
     tipe_pembayaran,
     status_pembayaran
   )
-VALUES ('P0001', 'Transfer Bank', 1),
-  ('P0002', 'Kartu Kredit', 1);
--- Insert into orders
+VALUES 
+  ('P0003', 'OVO', 1),
+  ('P0004', 'Dana', 1),
+  ('P0005', 'GoPay', 1),
+  ('P0006', 'LinkAja', 1),
+  ('P0007', 'ShopeePay', 1),
+  ('P0008', 'Cash On Delivery', 1),
+  ('P0009', 'Transfer BRI', 1),
+  ('P0010', 'Transfer BNI', 1),
+  ('P0011', 'Transfer Mandiri', 1),
+  ('P0012', 'Credit Card', 1);
+
+-- Insert additional data into orders
 INSERT INTO orders (
     id_order,
     jumlah_order,
@@ -324,34 +398,25 @@ INSERT INTO orders (
     id_penjual,
     id_pelanggan
   )
-VALUES (
-    'OR001',
-    2,
-    200000,
-    'Jalan Raya No. 123',
-    '2023-01-15',
-    'P0001',
-    'PR1',
-    'K1000',
-    'P1000',
-    'C1000'
-  ),
-  (
-    'OR002',
-    1,
-    150000,
-    'Jalan Utama No. 456',
-    '2023-01-16',
-    'P0002',
-    'PR2',
-    'K2000',
-    'P2000',
-    'C2000'
-  );
+VALUES 
+  ('OR003', 3, 300000, 'Jalan Maju No. 789', '2023-02-20', 'P0003', 'PR0003', 'K3000', 'P3000', 'C3000'),
+  ('OR004', 1, 80000, 'Jalan Sejahtera No. 1011', '2023-02-21', 'P0004', 'PR0004', 'K4000', 'P4000', 'C4000'),
+  ('OR005', 2, 500000, 'Jalan Harmoni No. 1213', '2023-02-22', 'P0005', 'PR0005', 'K5000', 'P5000', 'C5000'),
+('OR006', 1, 300000, 'Jalan Indah No. 1415', '2023-02-23', 'P0006', 'PR0006', 'K6000', 'P6000', 'C6000'),
+('OR007', 4, 720000, 'Jalan Damai No. 1617', '2023-02-24', 'P0007', 'PR0007', 'K7000', 'P7000', 'C7000'),
+('OR008', 2, 360000, 'Jalan Sentosa No. 1819', '2023-02-25', 'P0008', 'PR0008', 'K8000', 'P8000', 'C8000'),
+('OR009', 3, 600000, 'Jalan Bersama No. 2021', '2023-02-26', 'P0009', 'PR0009', 'K9000', 'P9000', 'C9000'),
+('OR010', 1, 150000, 'Jalan Kencana No. 2223', '2023-02-27', 'P0010', 'PR0010', 'K1000', 'P1000', 'C1000'),
+('OR011', 2, 700000, 'Jalan Sejati No. 2425', '2023-02-28', 'P0011', 'PR0011', 'K1100', 'P1100', 'C1100'),
+('OR012', 1, 120000, 'Jalan Bahagia No. 2627', '2023-03-01', 'P0012', 'PR0012', 'K1200', 'P1200', 'C1200');
 -- Insert into jasa_pengiriman
 INSERT INTO jasa_pengiriman (kode_pengiriman, nama_pengiriman, STATUS)
 VALUES ('JP001', 'Reguler', 1),
-  ('JP002', 'Express', 1);
+  ('JP002', 'Express', 1),
+  ('JP003', 'JNE', 0),
+  ('JP005', 'Tik Tok Express' , 0),
+  ('JP004', 'Gojek', 1);
+
 
 
 
@@ -558,7 +623,7 @@ SELECT
     P.id_produk,
     P.merk,
     K.nama_kategori,
-    CONCAT('Rp ', FORMAT(AVG(O.total_harga), 0)) AS avg_order_total_price,
+    CONCAT('Rp ', FORMAT(AVG(O.total_harga * jumlah_order), 0)) AS avg_order_total_price,
     COUNT(O.id_order) AS total_penjualan,
     CONCAT('Rating ', FORMAT(AVG(R.bintang), 1)) AS avg_rating
 FROM produk P
@@ -611,7 +676,6 @@ JOIN alamat A ON Pe.id_kode_pos = A.id_kode_pos
 WHERE A.id_kode_pos = 'contoh_id_kode_pos';
 
 -- 6. Penjual dengan penjualan terbanyak
-UNION ALL
 SELECT id_penjual, COUNT(*) AS total_penjualan
 FROM orders
 GROUP BY id_penjual
